bcb24d0fafe67d84bc9413a7cc13732007bb070f
Farrel Fanny <farrelexamples@gmail.com>
2024-01-23 12:40:48 +0700

    Initial commit

---
+++ src/app/card.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import DividerDesktop from "../../public/images/pattern-divider-desktop.svg";
+import DividerMobile from "../../public/images/pattern-divider-mobile.svg";
+import Dice from "../../public/images/icon-dice.svg";
+import { unstable_noStore as noStore } from "next/cache";
+
+const Card = () => {
+  const [adviceId, setAdviceId] = useState(null);
+  const [adviceWord, setAdviceWord] = useState("");
+
+  const fetchAdvice = async () => {
+    try {
+      let response = await fetch(`https://api.adviceslip.com/advice`);
+      const adviceData = await response.json();
+      const adviceObj = adviceData.slip;
+      setAdviceId(adviceObj.id);
+      setAdviceWord(adviceObj.advice);
+    } catch (error) {
+      console.error("There's an error =>", error);
+    }
+  };
+
+  useEffect(() => {
+    noStore(), fetchAdvice();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center pb-12 px-4 py-6 sm:p-6 rounded-xl max-w-md bg-Dark-Grayish-Blue gap-4 relative shadow-2xl">
+      <h5
+        className="text-Neon-Green text-sm tracking-widest"
+        suppressHydrationWarning={true}
+      >
+        Advice #{adviceId}
+      </h5>
+      <p
+        className="text-center text-Light-Cyan font-bold text-[26px]"
+        suppressHydrationWarning={true}
+      >
+        "{adviceWord}"
+      </p>
+      <Image
+        src={DividerDesktop}
+        alt="divider image"
+        className="sm:max-w-full hidden sm:flex"
+        priority
+      />
+      <Image
+        src={DividerDesktop}
+        alt="divider image"
+        className="max-w-full sm:hidden"
+        priority
+      />
+      <button
+        onClick={() => fetchAdvice()}
+        className="absolute -bottom-5 bg-Neon-Green hover:drop-shadow-[0px_0px_8px_#80ffbf] active:bg-Dark-Neon-Green active:drop-shadow-[0px_0px_8px_#00994d]  rounded-full w-10 h-10 z-10"
+      >
+        <div className="flex justify-center items-center w-full">
+          <Image src={Dice} alt="Dice icon" className="w-4 h-4" />
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Card;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "Light-Cyan": "hsl(193, 38%, 86%)",
+      "Neon-Green": "hsl(150, 100%, 66%)",
+      "Dark-Neon-Green": "hsl(150, 100%, 40%)",
+      "Grayish-Blue": "hsl(217, 19%, 38%)",
+      "Dark-Grayish-Blue": "hsl(217, 19%, 24%)",
+      "Dark-Blue": "hsl(218, 23%, 16%)",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
